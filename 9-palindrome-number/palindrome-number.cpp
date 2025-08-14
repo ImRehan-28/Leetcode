dec82d8a61b1a9237f50977b5fa8438063e87f36
ImRehan-28 <rehanquadary288@gmail.com>
2025-08-14 17:26:09 +0530

    Time: 7 ms (15.38%) | Memory: 8.6 MB (67.60%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,12 +3,9 @@ public:
     bool isPalindrome(int x) 
     {
         if(x<0 || (x%10 ==0 && x!=0))
-        {
             return false;
-        }
         else{
             int reversed=0;
-
             while(x>reversed)
             {
                 reversed = reversed *10+(x%10);
