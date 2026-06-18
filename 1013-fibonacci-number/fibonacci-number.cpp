0e98633a5f66a2499ae8e34b953cc0cbf287523e
Rehan Quadary <rehanquadary288@gmail.com>
2026-06-18 12:22:49 +0530

    Time: 14 ms (22.18%) | Memory: 7.7 MB (88.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -7,8 +7,8 @@ public:
         if(n==1)
         return 1;
 
-        
+        // int ans=fib(n-1)+fib(n-2);
 
-        return fib(n-1)+fib(n-2);;    
+        return fib(n-1)+fib(n-2); 
     }
 };
